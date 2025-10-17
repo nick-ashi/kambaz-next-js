186e05cb037e60b13903d711df7e0d78d14f5824
Nick Ashizawa <nickashi2002@gmail.com>
2025-10-16 20:01:16 -0400

    fixes for vercel deployment

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -22,6 +22,6 @@ export default function TOC() {
        <NavLink href="/" as={Link}>
          Kambaz </NavLink> </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/nick-ashi">My GitHub</NavLink></NavItem>
+       <NavLink href="https://github.com/nick-ashi/kambaz-next-js/tree/a3">My GitHub</NavLink></NavItem>
    </Nav>
  );}
