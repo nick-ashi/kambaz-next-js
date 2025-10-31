2100c4d6a4601e562e4fb072bc81e9f3b0e3e3cf
Nick Ashizawa <nickashi2002@gmail.com>
2025-10-31 15:46:18 -0400

    a4 prod build debugging

--- app/Labs/Lab4/ReduxExamples/CounterRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/CounterRedux/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./counterReducer";
 export default function CounterRedux() {
