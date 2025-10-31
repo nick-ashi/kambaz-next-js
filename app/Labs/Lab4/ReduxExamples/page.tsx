dd993488bab11f4573616dc516e3f13b48145ac3
Nick Ashizawa <nickashi2002@gmail.com>
2025-10-31 15:53:08 -0400

    a4 prod build debugging

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,16 +1,21 @@
+"use client";
+import store from "../store";
 import AddRedux from "./AddRedux/page";
 import CounterRedux from "./CounterRedux/page";
 import HelloRedux from "./HelloRedux/page";
 import TodoList from "./todos/TodoList";
+import { Provider } from "react-redux";
 
 export default function ReduxExamples() {
   return(
-    <div>
-      <h2>Redux Examples</h2>
-      <HelloRedux />
-      <CounterRedux />
-      <AddRedux />
-      <TodoList />
-    </div>
+    <Provider store={store}>
+      <div>
+        <h2>Redux Examples</h2>
+        <HelloRedux />
+        <CounterRedux />
+        <AddRedux />
+        <TodoList />
+      </div>
+    </Provider>
   );
 };
